cb56357217667c65d91f9b958c3ceffaf363bc5f
Quentin Langeveldt <quentinuys@gmail.com>
2015-10-10 00:46:51 +0200

    Small Change

--- lib/generators/spree_contact_us/templates/spree_contact_us.rb
+++ lib/generators/spree_contact_us/templates/spree_contact_us.rb
@@ -10,7 +10,7 @@ SpreeContactUs.setup do |config|
   config.mailer_from = nil
 
   # Configure the e-mail address which should receive the contact form email notifications.
-  config.mailer_to = "contact@please-change-me.com"
+  config.mailer_to = "support@zook.co.za"
 
   # ==> Form Configuration
 
